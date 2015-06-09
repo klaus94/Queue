3ae45862d3e694b914a0835490e3dd5e55a77553
klaus <supermagic999@web.de>
2015-06-09 20:50:04 +0200

    implementation of OrderedSetImpl and little bugfixes in OrderedSet

--- OrderedSet.java
+++ OrderedSet.java
@@ -1,14 +1,23 @@
 import java.util.Collection;
+import java.util.Set;
+import java.util.List;
 
-interface OrderedSet<T>
+
+interface OrderedSet<T> extends List<T>, Set<T>
 {
+	// Appends the specified element t to this set if this set does not contain an element other corresponding to other.equals(t).
 	public boolean add(T obj);
 
+	// Inserts the specified element t at the specified position in this set.
 	public void add(int i, T obj);
 
+	// Replaces the element at the specified position in this set with the specified element t 
+	// 		if the set does not contain an element other corresponding to other.equals(t).
 	public T set(int i, T obj);
 
-	public boolean addAll(Collection collection);
+	// Throws an UnsupportedOperationException upon invocation because this method is not supported.
+	public boolean addAll(Collection<? extends T> collection);
 
-	public boolean addAll(int i, Collection collection);
+	// Throws an UnsupportedOperationException upon invocation because this method is not supported.
+	public boolean addAll(int i, Collection<? extends T> collection);
 }
--- OrderedSetImpl.java
+++ OrderedSetImpl.java
@@ -1,29 +1,109 @@
 import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+//import java.util.Set;
 
-public class OrderedSetImpl<T> implements OrderedSet<T>
+public class OrderedSetImpl<T> extends ArrayList<T> implements OrderedSet<T>
 {
+	private List<T> orderedSet; 
+
+	// returns true if set contains obj
+	private boolean setContains(T obj)
+	{
+		for (T elem: orderedSet)
+		{
+			if (elem.equals(obj))
+				return true;
+		}
+
+		return false;
+	}
+
+	// IMPLEMENTATION OF ORDEREDSET:
 	public boolean add(T obj)
 	{
-		
+
+		if (obj == null)
+		{
+			throw new NullPointerException("Object should not be null!");
+		}
+
+		if (!setContains(obj))
+		{
+			orderedSet.add(obj);
+			return true;
+		}
+		else 
+		{
+			return false;	
+		}
+
 	}
 
 	public void add(int i, T obj)
 	{
 
+		if (obj == null)
+		{
+			throw new NullPointerException("Object should not be null!");
+		}
+
+
+		if (!setContains(obj))
+		{
+			orderedSet.add(i, obj);
+		}
+
 	}
 
+	// replaces the object at the index i with obj
 	public T set(int i, T obj)
 	{
+		if (obj == null)
+		{
+			throw new NullPointerException("Object should not be null!");
+		}
+
+		if (i >= orderedSet.size())
+		{
+			throw new IndexOutOfBoundsException("index was too big");
+		}
+
+		if (!setContains(obj))
+		{
+			T oldElem = orderedSet.get(i);
+			orderedSet.set(i, obj);
+
+			return oldElem;
+		}
+		else
+			return null;			// set contains obj already
 
 	}
 
-	public boolean addAll(Collection collection)
+	public boolean addAll(Collection<? extends T> collection)
 	{
-
+		throw new UnsupportedOperationException("not supported operation");
 	}
 
-	public boolean addAll(int i, Collection collection)
+	public boolean addAll(int i, Collection<? extends T> collection)
 	{
-
+		throw new UnsupportedOperationException("not supported operation");
 	}
+
+
+	// OWN METHODS:
+	public OrderedSetImpl()
+	{
+		orderedSet = new ArrayList<T>();
+	}
+
+	public OrderedSetImpl(int initialCapacity)
+	{
+		// TODO:
+		// how to fill orderedSet just with a given initialCapacity???
+		// adding this amount of null-objects cannot be right...
+	}
+
+
 }
