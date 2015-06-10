8e2acdd9295e72914f75331c17a9781dd0d88c5a
Tilo Werdin <werdin.tilo@arcor.de>
2015-06-10 09:46:43 +0200

    first draft of QueueImpl.java

--- QueueImpl.java
+++ QueueImpl.java
@@ -1,28 +1,56 @@
 public class QueueImpl<T> implements Queue<T>
 {
+	private List<T> myObjects;
+
+	public QueueImpl (boolean withDublicates) {
+		if (withDublicates) {
+			myObjects = new LinkedList<T>();
+		} else {
+			myObjects = new OrderedSetImpl<T>();
+		}
+	}
+
 	public boolean add(T o)
 	{
-
+		if (o == null) {
+			throw new NullPointerException("Obj darf nicht null sein.");
+		} else {
+			List<T> vergleich = myObjects;
+			myObjects.add(o);
+			if (myObjects.equals(o)) {
+				return false;
+			} else {
+				return true;
+			}
+		}
 	}
 
 	public T remove()
 	{
-
+		if (isEmpty()) {
+			return null;
+		} else {
+			return myObjects.remove(0);
+		}
 	}
 
 	public T front()
 	{
-
+		if (isEmpty()) {
+			return null;
+		} else {
+			return myObjects.get(0);
+		}
 	}
 
 	public boolean isEmpty()
 	{
-
+		return myObjects.isEmpty();
 	}
 
 	public int size()
 	{
-
+		return myObjects.size();
 	}
 	
 }
